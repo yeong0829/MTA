4812a48b2c22adaf82d7877d591825d06d64067a
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-17 17:52:52 +0900

    test7
    test7

---
+++ tset7.java
@@ -0,0 +1,16 @@
+package MTA;
+
+import java.util.Scanner;
+
+public class tset7 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println(s);
+		
+		for (int i = 0; i < 3; i++) {
+			int num = sc.nextInt();
+			System.out.println(num);
+		}
+	}
+}
