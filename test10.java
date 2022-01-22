be844a648f38b058c7ee7f6b32e0adcb1584fb35
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-22 14:45:49 +0900

    문제10
    문제10

---
+++ test10.java
@@ -0,0 +1,13 @@
+package MTA;
+
+public class test10 {
+
+	public static void main(String[] args) {
+		//String text = "" + number;			
+		//String text = " " + number;		
+		//String text = number.toString();	
+		//String text = String.value0f(number);	
+
+	}
+
+}
