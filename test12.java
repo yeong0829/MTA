965f4ae084175a39886d3057211522e30faf6e31
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-23 19:16:48 +0900

    문제12
    문제12

---
+++ test12.java
@@ -0,0 +1,23 @@
+//package MTA;
+//
+//public class test12 {
+//	int x = 25;
+//	public static void main(String[] args) {
+//		Test12 app = new Test12();
+//		{
+//			int x = 5;
+//		}
+//		{
+//			int x = 10;
+//		}
+//		int x = 100;
+//		System.out.println(x);
+//		System.out.println(app.x);
+//	}
+//	
+//	public Test12() {
+//		int x = 1;
+//		System.out.println(x);
+//	}
+//
+//}
