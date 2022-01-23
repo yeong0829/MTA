3845f435f2d207321f338ca952468cb3cd9e5323
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-23 19:18:50 +0900

    문제18
    문제18

---
+++ test18.java
@@ -0,0 +1,12 @@
+package MTA;
+
+public class test18 {
+	public static int negativeSqure(int number) {
+		return -(number * number);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(negativeSqure(7));
+	}
+
+}
