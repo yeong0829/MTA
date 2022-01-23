8e7e88299a4531905f7028b7cca5d01b343197ed
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-23 19:17:08 +0900

    문제13
    문제13

---
+++ test13.java
@@ -0,0 +1,17 @@
+package MTA;
+
+public class test13 {
+	private double balance; // ภÜพื
+	
+	test13(double startingBalance) {
+		this.balance = startingBalance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
