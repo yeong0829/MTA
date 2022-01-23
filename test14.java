7cf12ec13218a2097259964160d82c261679e70b
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-23 19:17:23 +0900

    문제14
    문제14

---
+++ test14.java
@@ -0,0 +1,13 @@
+//package MTA;
+//
+//public class test14 {
+//
+//	public static void main(String[] args) {
+//		test14 t1 = new test14();
+//		System.out.println("t1:"+t1.balance);
+//		
+//		test14 t2 = new test14(10);
+//		System.out.println("t2"+t2.balance);
+//	}
+//
+//}
