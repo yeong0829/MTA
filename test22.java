507fe97691a97a941ba6ebf0ca2c3023f096dd1e
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-24 15:52:01 +0900

    문제22
    문제22

---
+++ test22.java
@@ -0,0 +1,16 @@
+package MTA;
+
+public class test22 {
+
+	public static void main(String[] args) {
+		char data1 = 65;
+		System.out.println(data1);
+		long data2 = 65;
+		System.out.println(data2);
+		float data3 = new Float("-65.0");
+		System.out.println(data3);
+		short data4 = new Short("65.0");
+		System.out.println(data4);
+	}
+
+}
